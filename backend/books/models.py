d4f67cac47f0b386ef9b9aae673f7d01721ce2d2
Jan <Jangalcan@gmail.com>
2026-02-14 17:17:02 +0100

    Books + Authors REST API

---
+++ backend/books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    pages = models.IntegerField()
+    published_year = models.IntegerField()
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
+
+    def __str__(self):
+        return self.title
+
+
