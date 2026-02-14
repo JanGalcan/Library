86274617f341461b81329c73b0e071baf3ed9b28
Jan <Jangalcan@gmail.com>
2026-02-14 18:03:37 +0100

    první verze

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "nationality")
+    search_fields = ("first_name", "last_name")
+    list_filter = ("nationality",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "published_year", "genre")
+    search_fields = ("title",)
+    list_filter = ("genre", "published_year")
